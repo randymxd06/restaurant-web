38ebd5b15a70b1b06302659be1f3204cc262b551
Randy R. Martínez <55677075+randymxd06@users.noreply.github.com>
2022-01-20 22:34:59 -0400

    Initial commit

---
+++ resources/views/dashboard/index.blade.php
@@ -0,0 +1,19 @@
+@extends('adminlte::page')
+
+@section('title', 'Dashboard')
+
+@section('content_header')
+    <h1>Dashboard</h1>
+@stop
+
+@section('content')
+    <p>Bienvenido a este panel admin hermoso</p>
+@stop
+
+@section('css')
+    <link rel="stylesheet" href="/css/admin_custom.css">
+@stop
+
+@section('js')
+    <script>Console.log('HOLA');</script>
+@stop
