3277fef52605e27e9760f901e69f51195cd9aae3
DGuzmanUni <dauryuniversidad27@gmail.com>
2022-03-23 20:08:41 -0400

    Rutas de Caja en el Menu

--- config/adminlte.php
+++ config/adminlte.php
@@ -399,6 +399,25 @@ return [
             'icon' => 'fas fa-fw fa-truck-loading',
         ],
 
+        // Productos
+        [
+            'text' => 'Cajas',
+            // 'url'  => '#',
+            'icon' => 'fas fa-fw fa-cash-register',
+            'submenu' => [
+                [
+                    'text' => 'Gestionar Cajas',
+                    'url'  => 'box',
+                    'icon' => 'fas fa-boxes'
+                ],
+                [
+                    'text' => 'Agregar Cajas',
+                    'url'  => 'box/create',
+                    'icon' => 'fas fa-box'
+                ]
+            ]
+        ],
+
         // Compras
         [
             'text' => 'Compras',
