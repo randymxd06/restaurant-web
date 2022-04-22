3d4e624c0e55d00ab17a2c6e146a243470d4cb75
Randy R. Martínez <55677075+randymxd06@users.noreply.github.com>
2022-04-22 00:28:00 -0400

    Puse accesos directos en el dashboard, voy a poner una tabla de los pedidos

--- resources/views/dashboard/index.blade.php
+++ resources/views/dashboard/index.blade.php
@@ -7,65 +7,83 @@
 @stop
 
 @section('content')
+
 <section class="content">
-    <div class="row">
-        <div class="col-lg-3 col-6">
-            <div class="small-box bg-info">
-                <div class="inner">
-                    <h3>150</h3>
-                    <p>New Orders</p>
+
+    <div class="card container" style="border-radius: 10px">
+
+        <p class="my-2" style="font-size: 25px">Accesos directos</p>
+
+        <div class="row">
+
+            {{-- PEDIDOS --}}
+            <div class="col-lg-3 col-6">
+                <div class="small-box bg-success container">
+                    <a href="{{ url('/caja/create') }}" class="small-box-footer py-5">
+                        <div class="inner">
+                            <h3 class="text-center">Pedidos</h3>
+                        </div>
+                        <div class="icon">
+                            <i class="fas fa-shopping-bag mt-3"></i>
+                        </div>
+                    </a>
                 </div>
-                <div class="icon">
-                    <i class="fas fa-shopping-bag"></i>
-                </div>
-                <a href="#" class="small-box-footer">More info <i class="fas fa-arrow-circle-right"></i></a>
             </div>
+
+            {{-- RESERVACIONES --}}
+            <div class="col-lg-3 col-6">
+                <div class="small-box bg-warning container">
+                    <a href="{{ url('/reservation') }}" class="small-box-footer py-5">
+                        <div class="inner">
+                            <h3 class="text-center">Reservaciones</h3>
+                        </div>
+                        <div class="icon">
+                            <i class="fas fa-calendar-alt mt-3"></i>
+                        </div>
+                    </a>
+                </div>
+            </div>
+
+            {{-- COCINA --}}
+            <div class="col-lg-3 col-6">
+                <div class="small-box bg-info container">
+                    <a href="{{ url('/cocina') }}" class="small-box-footer py-5">
+                        <div class="inner">
+                            <h3 class="text-center">Cocina</h3>
+                        </div>
+                        <div class="icon">
+                            <i class="fas fa-hat-chef mt-3"></i>
+                        </div>
+                    </a>
+                </div>
+            </div>
+
+            {{-- REPORTES --}}
+            <div class="col-lg-3 col-6">
+                <div class="small-box bg-secondary container">
+                    <a href="#" class="small-box-footer py-5">
+                        <div class="inner">
+                            <h3 class="text-center">Reportes</h3>
+                        </div>
+                        <div class="icon">
+                            <i class="far fa-file-chart-line mt-3"></i>
+                        </div>
+                    </a>
+                </div>
+            </div>
+
         </div>
 
-        <div class="col-lg-3 col-6">
-            <div class="small-box bg-success">
-                <div class="inner">
-                    <h3>53<sup style="font-size: 20px">%</sup></h3>
-                    <p>Bounce Rate</p>
-                </div>
-                <div class="icon">
-                    <i class="fas fa-chart-bar"></i>
-                </div>
-                <a href="#" class="small-box-footer">More info <i class="fas fa-arrow-circle-right"></i></a>
-            </div>
-        </div>
-
-        <div class="col-lg-3 col-6">
-            <div class="small-box bg-warning">
-                <div class="inner">
-                    <h3>44</h3>
-                    <p>User Registrations</p>
-                </div>
-                <div class="icon">
-                    <i class="fas fa-user-plus"></i>
-                </div>
-                <a href="#" class="small-box-footer">More info <i class="fas fa-arrow-circle-right"></i></a>
-            </div>
-        </div>
-
-        <div class="col-lg-3 col-6">
-            <div class="small-box bg-danger">
-                <div class="inner">
-                    <h3>65</h3>
-                    <p>Unique Visitors</p>
-                </div>
-                <div class="icon">
-                    <i class="fas fa-chart-pie"></i>
-                </div>
-                <a href="#" class="small-box-footer">More info <i class="fas fa-arrow-circle-right"></i></a>
-            </div>
-        </div>
     </div>
+
+
+
 </section>
+
 @stop
 
 @section('css')
-    
+    <link rel="stylesheet" href="https://pro.fontawesome.com/releases/v5.10.0/css/all.css" integrity="sha384-AYmEC3Yw5cVb3ZcuHtOA93w35dYTsvhLPVnYs9eStHfGJvOvKxVfELGroGkvsg+p" crossorigin="anonymous"/>
     <link rel="stylesheet" href="{{ asset(mix('css/app.css')) }}">
 @stop
 
@@ -78,7 +96,7 @@
                 title: 'Error al acceder a Venta!',
                 text: 'Tu usuario no esta realacionado a una terminal de uso',
                 showConfirmButton: true,
-                confirmButtonText: 'Cerrar'  
+                confirmButtonText: 'Cerrar'
             })
         </script>
     @endif
